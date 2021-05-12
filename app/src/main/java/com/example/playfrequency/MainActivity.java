6985d8f11a54871abb6079f1662736c548c35aeb
miccu64 <98konrad98@gmail.com>
2021-05-12 15:13:21 +0200

    Added WebSockets

--- app/src/main/java/com/example/playfrequency/MainActivity.java
+++ app/src/main/java/com/example/playfrequency/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketFactory;
+import com.neovisionaries.ws.client.WebSocketFrame;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,8 +19,13 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -24,6 +34,9 @@ public class MainActivity extends AppCompatActivity implements Observer {
     private TextView frequencyView;
     private TextView magnitudeView;
     private CaptureAudioObservable audio;
+    private WebSocketFactory factory;
+    private WebSocket socket;
+    private TextView isConnected;
 
     private void updateElements(float freq, int magn) {
         //needed to update elements on UI from other thread
@@ -33,9 +46,48 @@ public class MainActivity extends AppCompatActivity implements Observer {
         });
     }
 
+    private void showToast(String text) {
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+
+    }
+
+    public void connect(View view) {
+        EditText serverIP = findViewById(R.id.editIP);
+        String serverName = serverIP.getText().toString();
+        factory.setServerName(serverName);
+        try {
+            socket = factory.createSocket(serverName);
+            // Register a listener to receive WebSocket events.
+            socket.addListener(new WebSocketAdapter() {
+                @Override
+                public void onTextMessage(WebSocket websocket, String message) {
+                    showToast(message);
+                }
+
+                @Override
+                public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
+                    super.onConnected(websocket, headers);
+                    isConnected.setText("Status: połączony");
+                }
+
+                @Override
+                public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
+                    super.onDisconnected(websocket, serverCloseFrame, clientCloseFrame, closedByServer);
+                    isConnected.setText("Status: rozłączony");
+                }
+            });
+        } catch (Exception e) {
+            showToast("Nie można połączyć.");
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        factory = new WebSocketFactory();
+
         setContentView(R.layout.activity_main);
         audio = new CaptureAudioObservable();
         audio.addObserver(this);
